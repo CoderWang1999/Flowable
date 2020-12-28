266d20e88ee1eb1f554f6f489475180ec7a309c2
Coder_WangChneyang <1351093262@qq.com>
2020-12-28 09:13:05 +0800

    Flowable+SpringBoot-请假流程

--- src/main/java/com/haiyang/flowable/controller/LeaveController.java
+++ src/main/java/com/haiyang/flowable/controller/LeaveController.java
@@ -40,92 +40,6 @@ public class LeaveController {
     @Autowired
     private HistoryService historyService;
 
-    /**
-     * 添加请假
-     *
-     * @param userId    用户Id
-     * @param day     请假天数
-     * @param descption 描述
-     */
-    @RequestMapping(value = "add")
-    @ResponseBody
-    public String addLeave(String userId, Integer day, String descption) {
-        String filePath = "processes/leave.bpmn20.xml";
-        String name = "leave";
-        DeploymentBuilder deploymentBuilder = repositoryService.createDeployment()
-                .addClasspathResource(filePath).name(name);
-        Deployment deployment = deploymentBuilder.deploy();
-        //acr_re_deployment表的id
-        String id = deployment.getId();
-
-        ProcessDefinitionQuery query = repositoryService.createProcessDefinitionQuery();
-        //搜索条件deploymentId
-        query.deploymentId(id);
-        //最新版本过滤
-        query.latestVersion();
-        //查询
-        ProcessDefinition definition = query.singleResult();
-        //act_re_procdef表的key
-        String key = definition.getKey();
-        //启动流程
-        HashMap<String, Object> map = new HashMap<>();
-        map.put("taskUser", userId);
-        map.put("day", day);
-        ProcessInstance processInstance = runtimeService.startProcessInstanceByKey(key, map);
-        return "提交成功.流程Id为：" + processInstance.getId();
-    }
-
-
-    /**
-     * 获取taskId
-     * @param userId
-     * @return
-     */
-    public String getTaskId(String userId) {
-        List<Task> tasks = taskService.createTaskQuery().taskAssignee(userId).orderByTaskCreateTime().desc().list();
-        try {
-            String id = tasks.get(tasks.size() - 1).getId();
-            return id;
-        }catch (ArrayIndexOutOfBoundsException e){
-            e.printStackTrace();
-            return "此申请已通过或已驳回！";
-        }
-    }
-
-    /**
-     * 批准
-     * @param userId 任务ID
-     */
-    @RequestMapping(value = "apply")
-    @ResponseBody
-    public String apply(String userId) {
-        String taskId = getTaskId(userId);
-        Task task = taskService.createTaskQuery().taskId(taskId).singleResult();
-        if (task == null) {
-            return "此申请已通过或已驳回！";
-        }
-        //通过审核
-        HashMap<String, Object> map = new HashMap<>();
-        map.put("outcome", "通过");
-        taskService.complete(taskId, map);
-        return "已批准";
-    }
-
-
-    /**
-     * 驳回
-     * @param userId
-     * @return
-     */
-    @ResponseBody
-    @RequestMapping(value = "reject")
-    public String reject(String userId) {
-        String taskId = getTaskId(userId);
-        HashMap<String, Object> map = new HashMap<>();
-        map.put("outcome", "驳回");
-        taskService.complete(taskId, map);
-        return "被驳回";
-    }
 
     /**
      * 部署流程
@@ -139,7 +53,6 @@ public class LeaveController {
             Deployment deployment = deploymentBuilder.deploy();
             //acr_re_deployment表的id
             String id = deployment.getId();
-
             ProcessDefinitionQuery query = repositoryService.createProcessDefinitionQuery();
             //搜索条件deploymentId
             query.deploymentId(id);
